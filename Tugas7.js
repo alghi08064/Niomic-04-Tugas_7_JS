030d83d623a97234a0af6d2c6eb96f0e5f851f97
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2019-12-29 01:54:22 +0300

    Tugas 7 JS
    Tugas ini untuk Niomic

---
+++ Tugas7.js
@@ -0,0 +1,12 @@
+var tinggi_badan = [80, 67, 75, 79, 44, 56, 63, 77, 64, 71];
+for(let i=0; i<=tinggi_badan.length; i++){
+      console.log(tinggi_badan[i])
+}
+
+for(let x of tinggi_badan){
+  console.log(x);
+}
+
+//pada perulangan biasa, kita harus memasukkan parameter-parameter untuk membatasi perulangan tersebut.
+
+//pada perulangan for..of tidak memerlukan parameter-parameter untuk membatasi perulangan tersebut.
